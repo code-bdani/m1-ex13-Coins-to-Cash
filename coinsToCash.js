2d21cf00ed63011fa31dddafde891e91d9180530
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-16 15:51:02 -0600

    Got MVP for exercise!

--- coinsToCash.js
+++ coinsToCash.js
@@ -4,18 +4,29 @@ const piggyBank = {
     nickles: prompt("How many nickles do you have?"),
     pennies: prompt("How many pennies do you have?")
 };
-console.log("Piggy Bank", piggyBank);
+
+// takes value of each key and outputs it to the DOM in a seperate div
+var quarterCounter = document.getElementById("quartersTally");
+quarterCounter.innerHTML = `<li>Number of quarters = ${piggyBank.quarters}</li>`;
+
+var dimesCounter = document.getElementById("dimesTally");
+dimesCounter.innerHTML = `<li>Number of dimes = ${piggyBank.dimes}</li>`;
+
+var nicklesCounter = document.getElementById("nicklesTally");
+nicklesCounter.innerHTML = `<li>Number of nickles = ${piggyBank.nickles}</li>`;
+
+var penniesCounter = document.getElementById("penniesTally");
+penniesCounter.innerHTML = `<li>Number of pennies = ${piggyBank.pennies}</li>`;
 
 let dollarAmount = 0;
-console.log("dollarAmount", dollarAmount);
 
-piggyBank.quarters = piggyBank.quarters * 0.25;
-piggyBank.dimes = piggyBank.dimes * 0.10;
-piggyBank.nickles = piggyBank.nickles * 0.05;
-piggyBank.pennies = piggyBank.pennies * 0.01;
-console.log("Value of quarters:", piggyBank.quarters, "Value of dimes:", piggyBank.dimes, "Value of nickles:", piggyBank.nickles, "Value of pennies:", piggyBank.pennies);
+piggyBank.quarters = (piggyBank.quarters * .25);
+piggyBank.dimes = (piggyBank.dimes * .10);
+piggyBank.nickles = (piggyBank.nickles * .05);
+piggyBank.pennies = (piggyBank.pennies * .01);
+console.log("Value of quarters:", piggyBank.quarters.toFixed(2), "Value of dimes:", piggyBank.dimes.toFixed(2), "Value of nickles:", piggyBank.nickles.toFixed(2), "Value of pennies:", piggyBank.pennies.toFixed(2));
 
-function getTotal() {
-    dollarAmount += (piggyBank.quarters + piggyBank.dimes + piggyBank.nickles + piggyBank.pennies) / 100;
-    return dollarAmount;
-};
+dollarAmount = (piggyBank.quarters + piggyBank.dimes + piggyBank.nickles + piggyBank.pennies);
+
+var totalOutput = document.getElementById("total");
+totalOutput.innerHTML = `<h2>Your total is: $${dollarAmount}!</h2>`;
